5719bbd6fbd24078b2f748e76ef6b4512a1d6e04
= <osmansfk53@gmail.com>
2023-10-10 18:14:58 +0300

    Added mongodb connection

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 
 const port = process.env.PORT;
-app.listen(port, () => {
+const database = require("./database");
+const connect = async () => {
+  await database.connect();
   console.log(`Backend Server listening on port ${port} .....`);
-});
+};
+app.listen(port, connect);
