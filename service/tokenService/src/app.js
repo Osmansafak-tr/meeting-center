8a03343dab82ceeb0ab55228c23a400e418c2466
= <osmansfk53@gmail.com>
2023-10-12 22:08:56 +0300

    Added mongodb connection to token service

--- service/tokenService/src/app.js
+++ service/tokenService/src/app.js
@@ -1,8 +1,16 @@
+// Third party imports
 const express = require("express");
 const app = express();
 require("dotenv").config();
-
+const bodyParser = require("body-parser");
+// My imports
 const port = process.env.PORT;
-app.listen(port, () => {
+const database = require("./database");
+
+app.use(bodyParser.json());
+
+const connect = async () => {
+  await database.connect();
   console.log(`Token Service listening on ${port} ......`);
-});
+};
+app.listen(port, connect);
---
+++ service/tokenService/src/database/index.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+exports.connect = async () => {
+  const uri = process.env.MONGODB_URI;
+  await mongoose.connect(uri);
+  console.log("Connected to MongoDb database ....");
+};
