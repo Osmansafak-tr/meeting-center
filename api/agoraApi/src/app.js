dfc3dc05c7ae9125f951f48836b5e27a82ff48ea
Osman Şafak <osmansfk53@gmail.com>
2023-11-20 14:30:34 +0300

    Added api key auth for agoraApi

--- api/agoraApi/src/app.js
+++ api/agoraApi/src/app.js
@@ -6,8 +6,14 @@ require("dotenv").config();
 // My Imports
 const port = process.env.PORT;
 const database = require("./database");
+const { apiAuth } = require("./middlewares/auth");
 
 app.use(bodyParser.json());
+app.use(apiAuth);
+
+app.get("/", (req, res) => {
+  return res.status(200).json("Main Page");
+});
 
 const startApp = async () => {
   await database.connect();
